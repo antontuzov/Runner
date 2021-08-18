6475a9494ce2ee5af6875fe9e321e27582cc22fa
AntonTuzov <antontuzovf@gmail.com>
2021-08-18 16:06:23 +0300

    TabBar

--- Runner/AppStuff/SceneDelegate.swift
+++ Runner/AppStuff/SceneDelegate.swift
@@ -5,6 +5,8 @@
 //  Created by Anton Tuzov on 17.08.2021.
 //
 
+
+
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
--- Runner/Controllers/TabBarViewController.swift
+++ Runner/Controllers/TabBarViewController.swift
@@ -12,21 +12,18 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
-        
         tabBar.tintColor = UIColor.label
         setupViewControllers()
         
     }
     private func setupViewControllers() {
         viewControllers = [
-            creatViwe(for: HomeViewController(), title: "Run", systemImage: "hare"),
-            creatViwe(for: HistoryViewController(), title: "Logs", systemImage: "clock")
+            createTabBar(for: HomeViewController(), title: "Run", systemImage: "hare"),
+            createTabBar(for: HistoryViewController(), title: "Logs", systemImage: "clock")
         ]
     }
     
-    private func creatViwe(for viewController: UIViewController, title: String, systemImage: String) -> UIViewController {
+    private func createTabBar(for viewController: UIViewController, title: String, systemImage: String) -> UIViewController {
         let iconSymbol = UIImage(systemName: systemImage)
         let selectedSymbol = UIImage(systemName: systemImage, withConfiguration: UIImage.SymbolConfiguration(weight: .bold))
         let tabBarItem = UITabBarItem(title: title, image: iconSymbol, selectedImage: selectedSymbol)
